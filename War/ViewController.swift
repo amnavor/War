0c8e8d094c7984fc8ec3747a04f0be919735fdfc
Amna <amnavor@gmail.com>
2016-07-13 21:40:12 -0700

    Flips

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func playRoundTapped(sender: UIButton) {
-        self.playRoundButton.setTitle("Play Round", forState: UIControlState.Normal)
+        self.firstCardImageView.image = UIImage(named: "card1")
     }
 
 }
